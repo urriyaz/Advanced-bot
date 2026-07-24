baa11d42029cee99137d3a0f317dd6483b057569
urriyaz <riyazbrand696@gmail.com>
2026-07-24 15:05:06 +0600

    Update murgi.js

--- scripts/cmds/murgi.js
+++ scripts/cmds/murgi.js
@@ -17,7 +17,7 @@ module.exports = {
     // বন্ধ করার কমান্ড
     if (args[0] === "off") {
       activeFun.delete(event.threadID);
-      return message.reply("𝐑𝐘𝐙 তোর রিয়েল আব্বু 😵💫");
+      return message.reply("Shiblu তোর রিয়েল আব্বু 😵💫");
     }
 
     // কাউকে মেনশন না করলে এরর দিবে
@@ -61,7 +61,7 @@ module.exports = {
     ];
 
     activeFun.set(event.threadID, true);
-    message.reply("𝐑𝐘𝐙 তোরে চুদা শুরু করলো");
+    message.reply("Shiblu তোরে চুদা শুরু করলো");
 
     let count = 0;
     const limit = 80; // কয়টা মেসেজ দিবে তার লিমিট
